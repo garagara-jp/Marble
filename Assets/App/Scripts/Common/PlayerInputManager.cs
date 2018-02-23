7aeec27ef2aadd4490689b93d1c574807bc9fdfe
garagara-jp <kramschrott@gmail.com>
2018-02-24 05:58:12 +0900

    諸々追加

---
+++ Assets/App/Scripts/Common/PlayerInputManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    public readonly Subject<bool>
+}
